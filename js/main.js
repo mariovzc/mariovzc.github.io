933bb1b53833648261433274c767bb7f5c1c2eba
Mario vizcaino <mario.vizcainos@gmail.com>
2017-12-14 19:01:30 -0500

    setup skills using javascript

--- js/main.js
+++ js/main.js
@@ -1,39 +1,34 @@
-$(".button-collapse").sideNav();
+function stack () {
+  let skills = [
+    'Ruby on rails',
+    'Javascript',
+    'HTML5',
+    'CSS3',
+    'Bootstrap',
+    'Materialize',
+    'Vuejs',
+    'Quasar',
+    'Git',
+    'Node',
+    'Heroku',
+    'Pusher.io',
+    'SQL',
+    'POO',
+    'MVC',
+    'SWIFT',
+    'JQuery',
+    'Objective-C',
+    'Ajax',
+    'Scrum'
 
-$(document).ready(function () {
-  $(document).on("scroll", onScroll);
-
-  $('.trans-element a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-    $(document).off("scroll");
-
-    $('.trans-element a').each(function () {
-      $(this).removeClass('active');
-    })
-    $(this).addClass('active');
-
-    var target = this.hash;
-    $target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': $target.offset().top +2
-    }, 500, 'swing', function () {
-      window.location.hash = target;
-      $(document).on("scroll", onScroll);
-    });
-  });
-});
-
-function onScroll(event){
-  var scrollPosition = $(document).scrollTop();
-  $('.trans-element a').each(function () {
-    var currentLink = $(this);
-    var refElement = $(currentLink.attr("href"));
-    if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-      $('.trans-element a').removeClass("active");
-      currentLink.addClass("active");
-    }
-    else{
-      currentLink.removeClass("active");
-    }
-  });
+  ]
+  let container = document.getElementsByClassName('stack')[0]
+  skills.forEach( (element) => {
+    let chip = document.createElement('div')
+    chip.className = 'chip green darken-2 white-text'
+    chip.appendChild(document.createTextNode(element))
+    container.appendChild(chip)
+  })
+  let x = '<div class="chip green darken-2 white-text">Phonegap</div>'
 }
+stack()
